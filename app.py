81b127c46982d615ce4754447325536bbc266c21
abduvohid26 <abdujalilovabduvohid6@gmail.com>
2025-01-18 09:33:28 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from telethon import TelegramClient
+from environs import env 
+env.read_env()
+api_id = env.int('API_ID')
+api_hash = env.str('API_HASH')
+
+phone = "+998882042629"
+
+client = TelegramClient('session_name', api_id, api_hash)
+
+async def main():
+    await client.start(phone=phone)
+
+    me = await client.get_me()
+    print(f"Foydalanuvchi: {me.username}")
+    print(f"Ism: {me.first_name}")
+    print(f"ID: {me.id}")
+    print(f"Telefon: {me.phone}")
+
+with client:
+    client.loop.run_until_complete(main())
